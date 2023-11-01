8f97648d2d20663af72dbbdd51033faef8962eb6
jordan <jrobertswebdev@gmail.com>
2023-11-01 17:00:39 -0500

    start weather

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "./header/header"
+import Weather from "./weather/weather"
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center">
       <Header/>
       <h1>SUP</h1>
+      <Weather />
     </main>
   )
 }
--- app/weather/weather.tsx
+++ app/weather/weather.tsx
@@ -1,9 +1,21 @@
+'use client'
+import { useEffect, useState } from "react"
+import Image from 'next/image'
 import React from 'react'
 
-function weather() {
+function Weather() {
+    const [weather, setWeather] = useState(null);
+    const icon =weather?.properties.periods[0].icon;
+    useEffect(() => {
+        fetch('https://api.weather.gov/gridpoints/SGF/67,31/forecast')
+        .then(response => response.json())
+        .then(data => setWeather(data));
+    }, [])
   return (
-    <div>weather</div>
+    <div>
+        <Image src=""/>
+    </div>
   )
 }
 
-export default weather
+export default Weather
