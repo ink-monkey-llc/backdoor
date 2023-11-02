e8715465d48c3adc5f1d989bb414aa0aa892ccac
jordan <jrobertswebdev@gmail.com>
2023-11-02 16:55:07 -0500

    adjust menus

--- app/header/menu.tsx
+++ app/header/menu.tsx
@@ -1,15 +1,18 @@
 interface MenuProps {
-    links: { name: string; url: string; }[] | undefined
-    }
+  links: { name: string; url: string }[] | undefined
+}
 
-const Menu = ({ links}: MenuProps) => {
+const Menu = ({ links }: MenuProps) => {
   return (
-    <div className='w-full absolute bg-[#2b2c2c]'>
+    <div className='w-full absolute py-4 bg-[#2b2c2c]'>
+      {/* <div className='h-[1px] bg-white w-full' /> */}
       {links &&
         links.map((link) => (
-          <a href={link.url} key={link.name} className='block text-white text-center py-2 hover:bg-[#8e4f0a] hover:text-black'>
-            {link.name}
-          </a>
+          <>
+            <a href={link.url} key={link.name} className='block text-white text-start py-1 px-4 hover:bg-[#333]'>
+              {link.name}
+            </a>
+          </>
         ))}
     </div>
   )
