49586d02fb468901706c0f1f3563e7311cd3b0d7
Jordan Roberts <jrobertswebdev@gmail.com>
2024-08-09 12:41:11 -0500

    hover effect for header nav buttons

--- app/header/btn.tsx
+++ app/header/btn.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 function Btn({ children, onClick }: { children: React.ReactNode; onClick: () => void }) {
  return (
-  <div
+  <button
    onClick={onClick}
-   className='cursor-pointer relative items-center justify-center w-36 flex h-8 border bg-[#2b2c2c] border-white border-opacity-30  overflow-hidden  text-white  rounded-sm group'>
-   <span className='absolute w-0 h-0 transition-all duration-500 ease-out bg-accent rounded-full group-hover:w-56 group-hover:h-56'></span>
-   <span className='absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 '></span>
-   <span className='relative text-sm group-hover:text-slate-900 group-hover:font-bold transition-all'>{children}</span>
-  </div>
+   className='cursor-pointer relative items-center justify-center w-36 flex h-8 border bg-[#2b2b2c] border-white border-opacity-30  overflow-hidden  text-white  rounded-sm group'>
+   <span className='relative text-sm '>{children}</span>
+   <div className='absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]'>
+    <div className='relative h-full w-8 bg-white/20'></div>
+   </div>
+  </button>
  )
 }
 
 export default Btn
-
-//  className='flex h-8 border bg-[#2b2c2c] border-opacity-30 border-white text-sm w-36 justify-center items-center rounded-sm'
